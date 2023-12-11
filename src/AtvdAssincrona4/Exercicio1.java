6b403c891e19050f14b357bf3197704ddccf89df
taisarantes <tais.reis.arantes@gmail.com>
2023-12-11 20:01:01 -0300

    correção exercicio 1

--- src/AtvdAssincrona4/Exercicio1.java
+++ src/AtvdAssincrona4/Exercicio1.java
@@ -3,28 +3,29 @@ package AtvdAssincrona4;
 import java.util.Scanner;
 
 public class Exercicio1 {
-	public static void main(String[] args) {
-		int vetorNums[] = {2, 5, 1, 3, 4, 9, 7, 8, 10, 6};
-		int numUsuario;
-		boolean temNum = false;
-		Scanner leia = new Scanner(System.in);
-		
-		System.out.println("Digite o número que você deseja encontrar: ");
-		numUsuario = leia.nextInt();
-		
-		for(int i = 0; i < vetorNums.length; i++) {
-			if(vetorNums[i] == numUsuario) {
-				System.out.printf("O número %d está na posição: %d", 
-						numUsuario, i);
-				temNum = true;
-			} else {
-				if(temNum == false && vetorNums[i] == vetorNums[(vetorNums.length - 1)]) {
-					System.out.printf("O número %d não foi encontrado! Tente outro",
-							numUsuario);
-				}
-			}
-		}
+	 public static void main(String[] args) {
+	        Scanner leia = new Scanner(System.in);
 
-	}
+	        int[] vetorNums = {2, 5, 1, 3, 4, 9, 7, 8, 10, 6};
+	        int index = 0;
+	        boolean temNum = false;
+
+	        System.out.println("Digite o numero que você deseja encontrar:");
+	        int numero = leia.nextInt();
+
+	        while (index < vetorNums.length) {
+	            if (vetorNums[index] == numero) {
+	                temNum = true;
+	                break;
+	            }
+	            index++;
+	        }
+
+	        if (temNum) {
+	            System.out.printf("O número %d está na posição: %d", numero, index);
+	        } else {
+	            System.out.printf("O numero %d não foi encontrado! Tente outro", numero);
+	        }
+	    }
 	
 }
