510ae881454ead16b3f9880e19c9719652e58c29
taisarantes <tais.reis.arantes@gmail.com>
2023-12-11 20:05:20 -0300

    correção exercicio 1

--- src/AtvdAssincrona4/Exercicio1.java
+++ src/AtvdAssincrona4/Exercicio1.java
@@ -4,28 +4,28 @@ import java.util.Scanner;
 
 public class Exercicio1 {
 	 public static void main(String[] args) {
-	        Scanner leia = new Scanner(System.in);
+		 int vetorNums[] = {2, 5, 1, 3, 4, 9, 7, 8, 10, 6};
+		 int numUsuario;
+	     int index = 0;
+	     boolean temNum = false;
+	     Scanner leia = new Scanner(System.in);
 
-	        int[] vetorNums = {2, 5, 1, 3, 4, 9, 7, 8, 10, 6};
-	        int index = 0;
-	        boolean temNum = false;
+	     System.out.println("Digite o numero que você deseja encontrar: ");
+	     numUsuario = leia.nextInt();
 
-	        System.out.println("Digite o numero que você deseja encontrar:");
-	        int numero = leia.nextInt();
-
-	        while (index < vetorNums.length) {
-	            if (vetorNums[index] == numero) {
-	                temNum = true;
-	                break;
-	            }
-	            index++;
-	        }
-
-	        if (temNum) {
-	            System.out.printf("O número %d está na posição: %d", numero, index);
-	        } else {
-	            System.out.printf("O numero %d não foi encontrado! Tente outro", numero);
-	        }
-	    }
+	     while (index < vetorNums.length) {
+	    	 if (vetorNums[index] == numUsuario) {
+	    		 temNum = true;
+	             break;
+	         }
+	         index++;
+	     }
+	     
+	     if (temNum) {
+	    	 System.out.printf("O número %d está na posição: %d", numUsuario, index);
+	     } else {
+	         System.out.printf("O numero %d não foi encontrado! Tente outro", numUsuario);
+	     }
+	 }
 	
 }
